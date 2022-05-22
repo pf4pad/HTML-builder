4fb3e4d1f7b49105de177d44ed64a14f338c9920
Pavel Cherevach <pf4pad@gmail.com>
2022-05-22 19:13:26 +0300

    feat: create 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+const process = require('process');
+
+const pathToCreateFile = path.join(__dirname, 'text.txt');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.write('Enter your quetion:\n');
+appendFile();
+rl.on('line', createLine);
+rl.on('close', close);
+process.on('SIGINT', close);
+
+function appendFile(input = '') {
+  fs.appendFile(pathToCreateFile, input, function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+}
+
+function createLine(line) {
+  const answer = line.trim();
+  if (answer === 'exit') {
+    rl.close;
+  } else {
+    appendFile(answer + '\n');
+  }
+}
+function close() {
+  rl.removeListener('line', createLine);
+  rl.write('All done, thx.\n');
+  rl.pause();
+}
+
+appendFile();
