2e9e914e31ab988d104376d2863facd95fef884d
Pavel Cherevach <pf4pad@gmail.com>
2022-05-22 18:17:32 +0300

    feat: create 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+const process = require('process');
+
+function readFile() {
+  try {
+    const stream = fs.createReadStream(path.join(__dirname, 'text.txt'), {
+      encoding: 'utf-8',
+    });
+    stream.on('data', data => {
+      process.stdout.write(data.toString());
+    });
+  }
+  catch (err) {
+    console.log(err);
+  }
+}
+
+readFile();
