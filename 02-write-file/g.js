6b52ffe321a317ff6b130476df1c4dc948ce63c1
Pavel Cherevach <pf4pad@gmail.com>
2022-05-22 19:03:04 +0300

    feat: create 02-write-file

---
+++ 02-write-file/g.js
@@ -0,0 +1,39 @@
+const readline = require('readline');
+const path = require('path');
+const process = require('process');
+const fs = require('fs');
+const pathToOutPutFile = path.join(__dirname, 'answer.txt');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.write('Name your favorite movies:\n');
+appendFile();
+rl.on('line', createLine);
+rl.on('close', close);
+process.on('SIGINT', close);
+
+function createLine(line) {
+  const answer = line.trim().toLowerCase();
+  if (answer === 'exit') {
+    rl.close();
+  } else {
+    appendFile(answer + '\n');
+  }
+}
+
+function appendFile(input = '') {
+  fs.appendFile(pathToOutPutFile, input, function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+}
+
+function close() {
+  rl.removeListener('line', createLine);
+  rl.write('We are done for today. Bye-bye!\n');
+  rl.pause();
+}
